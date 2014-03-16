7dd23e14185b41bffd486bf3d0b25e0fd24e898e
Karl Giesing <khzmusik@hotmail.com>
2014-03-16 10:30:36 -0400

    Changed Doxygen style
    Doxygen blocks now use "@" instead of "\" as tag prefix.
    This setting can be changed in Code::Blocks by going to:
    DoxyBlocks->Open preferences...->General->Use "@" In Tags

--- include/ConcreteBase.h
+++ include/ConcreteBase.h
@@ -6,26 +6,26 @@ class ConcreteBase
 {
     public:
         /** Constructor
-         * \param Value
+         * @param Value
          */
         ConcreteBase(int);
         /** Destructor */
         virtual ~ConcreteBase();
         /** Copy constructor
-         *  \param other Object to copy from
+         *  @param other Object to copy from
          */
         ConcreteBase(const ConcreteBase& other);
         /** Assignment operator
-         *  \param other Object to assign from
-         *  \return A reference to this
+         *  @param other Object to assign from
+         *  @return A reference to this
          */
         ConcreteBase& operator=(const ConcreteBase& other);
         /** Access value
-         * \return The current value
+         * @return The current value
          */
         virtual int getVal() { return _val; }
         /** Set value
-         * \param val New value to set
+         * @param val New value to set
          */
         virtual void setVal(int val) { _val = val; }
     protected:
--- include/ConcreteDerived.h
+++ include/ConcreteDerived.h
@@ -8,27 +8,27 @@ class ConcreteDerived : public ConcreteBase
 {
     public:
         /** Constructor
-         * \param Value
-         * \param Scale
+         * @param Value
+         * @param Scale
          */
         ConcreteDerived(int, double=1.0);
         /** Destructor */
         ~ConcreteDerived();
         /** Copy constructor
-         *  \param other Object to copy from
+         *  @param other Object to copy from
          */
         ConcreteDerived(const ConcreteDerived& other);
         /** Assignment operator
-         *  \param other Object to assign from
-         *  \return A reference to this
+         *  @param other Object to assign from
+         *  @return A reference to this
          */
         ConcreteDerived& operator=(const ConcreteDerived& other);
         /** Access scaled value
-         * \return The current value multiplied by the scale
+         * @return The current value multiplied by the scale
          */
         virtual int getVal() { return _val * _scale; }
         /** Set value
-         * \param val New value to set
+         * @param val New value to set
          */
         void setScale(double scale) { _scale = scale; }
     private:
